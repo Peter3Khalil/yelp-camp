f8aa3da4437566b55e9c0f8f30f23beb99116c5d
Peter Khalil <rikokamil97@gmail.com>
2024-02-22 04:06:05 +0200

    Edit hero image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,14 @@ const Home = () => {
         <Image src="/Assets/logo.svg" alt="logo" width={100} height={100} />
       </header>
       <section className="relative h-[45vh] w-full shrink-0">
-        <Image src="/Assets/Hero Image (Tablet and Mobile).jpg" alt="hero" layout="fill" objectFit="cover" />
+        <Image src="/Assets/hero-mobile.jpg" alt="hero" layout="fill" objectFit="cover" />
       </section>
-      <article className="flex text-xl w-full shrink-0 flex-col gap-4 px-4 pt-6 text-left">
+      <article className="flex text-lg w-full shrink-0 flex-col gap-4 px-4 pt-6 text-left">
         <h1 className="text-4xl font-bold">Explore the best camps on Earth.</h1>
-        <p className="font-semibold text-muted-foreground">
+        <p className="font-medium text-muted-foreground">
           YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.
         </p>
-        <ul className="flex flex-col gap-4 font-semibold text-muted-foreground">
+        <ul className="flex flex-col gap-4 font-medium text-muted-foreground">
           {['Add your own camp suggestions.', 'Leave reviews and experiences.', 'See locations for all camps.'].map(
             (item, index) => (
               <li key={index} className="flex items-center gap-2">
